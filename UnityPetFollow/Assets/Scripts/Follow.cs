3f21fc84ba53f675cad80d886a7fc86caf11d8a8
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2019-12-25 17:36:07 +0800

    還沒完成

---
+++ UnityPetFollow/Assets/Scripts/Follow.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    [Header("跟隨速度"), Range(1, 10)]
+    public float speed;
+
+    private Transform Op;
+
+    private void track()
+    {
+
+        Vector3 posOp = Op.position;
+        Vector3 posFg = transform.position;
+
+        transform.position = Vector3.Lerp(posOp, posFg, 0.1f * Time.deltaTime * speed);
+        transform.LookAt(posOp);
+
+    }
+
+    private void Start()
+    {
+        Op = GameObject.Find("野蠻人").transform;
+    }
+
+
+    private void LateUpdate()
+    {
+        track();
+    }
+}
+
